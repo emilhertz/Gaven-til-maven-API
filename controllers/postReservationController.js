269c7c99357ac0285fa10c8c0814e1cb56d197de
pvburleigh <pvburleigh@gmail.com>
2020-04-19 11:02:08 +0200

    reservation controller now works. However it doesn't check doublebookings etc.

--- controllers/postReservationController.js
+++ controllers/postReservationController.js
@@ -1,3 +1,19 @@
+const Reservation = require("../models/Reservation");
 module.exports = (req, res) => {
+    Reservation.create({
+        ...req.body,
+        userId: req.userData.userId
+    })
+    //status code 201: "User created"
+        .then(response => {return res.status(201).json({
+            created: true
+        })})
+        .catch(error => {
+            console.log(error.message);
+            return res.status(401).json({
+                errors: error.message
+            })
+        })
+
 
 };
